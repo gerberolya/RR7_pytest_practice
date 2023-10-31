c546ad079155f2cfa56eb1041227cb884d76981e
Olga Gerber <gerberolya@gmail.com>
2023-10-31 12:32:16 +0100

    (+) base_page.py

---
+++ lesson1/pages/base_page.py
@@ -0,0 +1,12 @@
+
+
+
+
+class BasePage:
+
+    def __init__(self, driver, url):
+        self.driver = driver
+        self.url = url
+
+    def open_browser(self):
+        self.driver.get(self.url)
